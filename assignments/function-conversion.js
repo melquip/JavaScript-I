85a3fd54e055fbfe981134e743b93ce2d63c918b
Melquisedeque Pereira <melqui1@live.com.pt>
2019-08-19 12:23:33 +0100

    Finished MVP function-conversion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,24 +1,32 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
-
+console.info("\n\nfunction-conversion.js file\n\n");
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
 // myFunction();
-
+const myFunction = () => console.log("Function was invoked!");
+myFunction();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
-
+const anotherFunction = (param) => param;
+anotherFunction("Example");
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-
+const add = (param1, param2) => param1 + param2;
+add(1,2);
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+const subtract = (param1, param2) => param1 - param2;
+subtract(1,2);
+
+
+
 
 
 // Stretch
@@ -28,3 +36,5 @@
 //   return num * 3;
 // });
 // console.log(triple);
+const triple = (numbers) => numbers.map((num) => num * 3);
+console.log(triple);
